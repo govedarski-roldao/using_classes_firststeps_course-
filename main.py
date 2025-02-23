42a9767183e8a8906a06d0f56514f38e3a7639b1
Rodolfo Govedarski-Roldão <r_roldao@hotmail.com>
2025-02-24 00:20:09 +0200

    second steps

---
+++ main.py
@@ -0,0 +1,6 @@
+import folium
+
+azores = folium.folium.Map(location=(38,-27),
+                           zoom_start=6)
+
+print (type(azores))
---
+++ second_step.py
@@ -0,0 +1,18 @@
+class Paint:
+    def __init__(self, buckets, color):
+        self.buckets = buckets
+        self.color = color
+
+
+class House:
+    def __init__(self, wall_area):
+        self.area = wall_area
+
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#
+# point1 = Point(10,20)
+# print(type(point1))
+#
