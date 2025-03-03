58b800f2c7bb6b190c0c267e10d561d7c11a8a45
Rodolfo Govedarski-Roldão <r_roldao@hotmail.com>
2025-03-03 22:48:58 +0200

    new lines

--- second_step.py
+++ second_step.py
@@ -1,18 +1,28 @@
-class Paint:
-    def __init__(self, buckets, color):
-        self.buckets = buckets
-        self.color = color
-
-
-class House:
-    def __init__(self, wall_area):
-        self.area = wall_area
-
-# class Point:
-#     def __init__(self, x, y):
-#         self.x = x
-#         self.y = y
+# class Paint:
+#     def __init__(self, buckets, color):
+#         self.buckets = buckets
+#         self.color = color
 #
-# point1 = Point(10,20)
-# print(type(point1))
 #
+# class House:
+#     def __init__(self, wall_area):
+#         self.area = wall_area
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def falls_in_rectangle(self, lowleft, upright):
+        if lowleft[0] < self.x < upright[0] and lowleft[1] < self.y < upright[1]:
+            return True
+        else:
+            return False
+
+    def distance(self, x, y):
+        return abs(x - y)
+
+
+point1 = Point(10, 20)
+print(type(point1))
+print(Point(3, 4).falls_in_rectangle((5, 6), (7, 9)))
