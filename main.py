6f4a2590f62ce6cea4c934ea89a048bba3992c3b
Rodolfo Govedarski-Roldão <r_roldao@hotmail.com>
2025-03-29 14:17:19 +0200

    finish oprijext with paint buckets and rectangle

--- main.py
+++ main.py
@@ -1,37 +1,81 @@
-# import folium
-#
-# azores = folium.folium.Map(location=(38,-27),
-#                            zoom_start=6)
-#
-# print (type(azores))
-#
-# import turtle
-# from second_step import Rectangle
-#
-#
-# class DrawRectangle(Rectangle):
-#     def __init__(self, rec):
-#         self.starting_position = rec.lowleft
-#         self.horizontal = rec.upright.x - rec.lowleft.x
-#         self.vertical = rec.upright.y - rec.lowleft.y
-#         self.my_turtle = turtle.Turtle()
-#
-#     def draw(self):
-#         self.my_turtle.penup()
-#         self.my_turtle.goto(self.starting_position.x, self.starting_position.y)
-#         self.my_turtle.pendown()
-#         self.my_turtle.forward(self.horizontal)
-#         self.my_turtle.left(90)
-#         self.my_turtle.forward(self.vertical)
-#         self.my_turtle.left(90)
-#         self.my_turtle.forward(self.horizontal)
-#         self.my_turtle.left(90)
-#         self.my_turtle.forward(self.vertical)
-#         self.my_turtle.penup()
-#
-#         turtle.done()
-#
-#     def point_guessed(self, value):
-#         self.my_turtle.goto(value.x, value.y)
+from random import randint
+import turtle
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def falls_in_rectangle(self, rectangle):
+        if rectangle.lowleft.x < self.x < rectangle.upright.x and rectangle.lowleft.y < self.y < rectangle.upright.y:
+            return True
+        else:
+            return False
+
+    def distance(self, point):
+        return ((self.x - point.x) ** 2 + (self.y - point.y) ** 2) ** 0.5
 
 
+point1 = Point(6, 7)
+point2 = Point(2, 2)
+
+
+class Rectangle:
+    def __init__(self, lowleft, upright):
+        self.lowleft = lowleft
+        self.upright = upright
+
+    def area(self):
+        area = (self.upright.x - self.lowleft.x) * (self.upright.y - self.lowleft.y)
+        return area
+
+    def guessed_area(self, guess):
+        area = float(self.area())
+        if guess > area:
+            print(f"You choose to high, the area was {area}")
+        elif guess < area:
+            print(f"You choose to low, the area was {area}")
+        else:
+            print("Correct")
+
+
+class GuiRectangle(Rectangle):
+    def draw(self, canvas):
+        self.starting_position = self.lowleft
+        self.horizontal = self.upright.x - self.lowleft.x
+        self.vertical = self.upright.y - self.lowleft.y
+        canvas.penup()
+        canvas.goto(self.starting_position.x, self.starting_position.y)
+        canvas.pendown()
+        canvas.forward(self.horizontal)
+        canvas.left(90)
+        canvas.forward(self.vertical)
+        canvas.left(90)
+        canvas.forward(self.horizontal)
+        canvas.left(90)
+        canvas.forward(self.vertical)
+        canvas.penup()
+
+
+class GuiPoint(Point):
+    def mark_point(self, canvas, size=5, color='red'):
+        canvas.penup()
+        canvas.goto(self.x, self.y)
+        canvas.pendown()
+        canvas.dot(size, color)
+
+
+gui_rectangle = GuiRectangle(Point(randint(-200, 200), randint(-200, 200)), Point(randint(100, 190), randint(100, 190)))
+
+# Get the choice from user
+user_point = GuiPoint(float(input("Guess X:")), float(input("Guess Y:")))
+guess_area = float(input("Please, try to guess the area of the rectangle:"))
+# print the same result
+print("Your point was inside of the rectangle: ", user_point.falls_in_rectangle(gui_rectangle))
+print("Your area was off by: ", gui_rectangle.area() - guess_area)
+
+# Construct the gui
+myturle = turtle.Turtle()
+gui_rectangle.draw(myturle)
+user_point.mark_point(myturle)
+turtle.done()
--- second_step.py
+++ second_step.py
@@ -1,85 +1,4 @@
-from random import randint
-import turtle
 
-#
-# class Point:
-#     def __init__(self, x, y):
-#         self.x = x
-#         self.y = y
-#
-#     def falls_in_rectangle(self, rectangle):
-#         if rectangle.lowleft.x < self.x < rectangle.upright.x and rectangle.lowleft.y < self.y < rectangle.upright.y:
-#             return True
-#         else:
-#             return False
-#
-#     def distance(self, point):
-#         return ((self.x - point.x) ** 2 + (self.y - point.y) ** 2) ** 0.5
-#
-#
-# point1 = Point(6, 7)
-# point2 = Point(2, 2)
-#
-#
-# class Rectangle:
-#     def __init__(self, lowleft, upright):
-#         self.lowleft = lowleft
-#         self.upright = upright
-#
-#     def area(self):
-#         area = (self.upright.x - self.lowleft.x) * (self.upright.y - self.lowleft.y)
-#         return area
-#
-#     def guessed_area(self, guess):
-#         area = float(self.area())
-#         if guess > area:
-#             print(f"You choose to high, the area was {area}")
-#         elif guess < area:
-#             print(f"You choose to low, the area was {area}")
-#         else:
-#             print("Correct")
-#
-#
-# class GuiRectangle(Rectangle):
-#     def draw(self, canvas):
-#         self.starting_position = self.lowleft
-#         self.horizontal = self.upright.x - self.lowleft.x
-#         self.vertical = self.upright.y - self.lowleft.y
-#         canvas.penup()
-#         canvas.goto(self.starting_position.x, self.starting_position.y)
-#         canvas.pendown()
-#         canvas.forward(self.horizontal)
-#         canvas.left(90)
-#         canvas.forward(self.vertical)
-#         canvas.left(90)
-#         canvas.forward(self.horizontal)
-#         canvas.left(90)
-#         canvas.forward(self.vertical)
-#         canvas.penup()
-#
-#
-# class GuiPoint(Point):
-#     def mark_point(self, canvas, size=5, color='red'):
-#         canvas.penup()
-#         canvas.goto(self.x, self.y)
-#         canvas.pendown()
-#         canvas.dot(size, color)
-#
-#
-# gui_rectangle = GuiRectangle(Point(randint(-200, 200), randint(-200, 200)), Point(randint(100, 190), randint(100, 190)))
-#
-# # Get the choice from user
-# user_point = GuiPoint(float(input("Guess X:")), float(input("Guess Y:")))
-# guess_area = float(input("Please, try to guess the area of the rectangle:"))
-# # print the same result
-# print("Your point was inside of the rectangle: ", user_point.falls_in_rectangle(gui_rectangle))
-# print("Your area was off by: ", gui_rectangle.area() - guess_area)
-#
-# # Construct the gui
-# myturle = turtle.Turtle()
-# gui_rectangle.draw(myturle)
-# user_point.mark_point(myturle)
-# turtle.done()
 
 class Paint:
     def __init__(self, buckets, color):
@@ -101,10 +20,19 @@ class House:
         return self.area * 2.5
 
 
+class DiscountedPaint(Paint):
+   def discounted_price(self,discount_percentage):
+       price_after_discount = self.total_price() - (self.total_price() * (0.01*discount_percentage))
+       return price_after_discount
+
+
+
 first_house = House(50)
 area = first_house.paint_needed()
 print(area)
-new_price = Paint(5, "yellow")
-price = new_price.total_price()
-print(price)
+new_price = DiscountedPaint(5, "yellow")
+print(new_price)
+price_after_discount = new_price.discounted_price(20)  # 20% de desconto = 0.20
+print(price_after_discount)
+
 
